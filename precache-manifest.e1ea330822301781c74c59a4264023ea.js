d05a6f96f50822643dfc57bb533e1d927c4db564
tero-yrjola <yrjola.tero@gmail.com>
2018-12-16 22:42:34 +0200

    Updates

---
+++ precache-manifest.e1ea330822301781c74c59a4264023ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69696ef38fa838bd86f8",
+    "url": "/2018-interactive-web-apps-project-tero-yrjola/static/js/runtime~main.69696ef3.js"
+  },
+  {
+    "revision": "8671f75f5ab9bde6b8b3",
+    "url": "/2018-interactive-web-apps-project-tero-yrjola/static/js/main.8671f75f.chunk.js"
+  },
+  {
+    "revision": "45ad818947dced6bb5d0",
+    "url": "/2018-interactive-web-apps-project-tero-yrjola/static/js/1.45ad8189.chunk.js"
+  },
+  {
+    "revision": "8671f75f5ab9bde6b8b3",
+    "url": "/2018-interactive-web-apps-project-tero-yrjola/static/css/main.560995b6.chunk.css"
+  },
+  {
+    "revision": "a515db8c498581c568187a4231b5a144",
+    "url": "/2018-interactive-web-apps-project-tero-yrjola/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/2018-interactive-web-apps-project-tero-yrjola/precache-manifest.e1ea330822301781c74c59a4264023ea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/2018-interactive-web-apps-project-tero-yrjola/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
